ee0c260ece1e52f7423cd8c21d78f6b2768342c8
Hyun Woo Krassilchikoff <hkrassil@gmail.com>
2025-10-03 19:32:22 +0200

    wiring of post

--- projects/zoneless-app-v2/src/app/modules/post/pages/posts/posts.component.ts
+++ projects/zoneless-app-v2/src/app/modules/post/pages/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Signal, signal, WritableSignal } from "@angular/core";
+import { Component, effect, inject, Signal, signal, WritableSignal } from "@angular/core";
 import { PostService } from "../../services/post.service";
 import { CommonModule } from "@angular/common";
 import { Post } from "../../models/post.model";
@@ -13,13 +13,19 @@ import { PostDialogComponent } from "../../components/post-dialog/post-dialog.co
 export class PostsComponent {
     private postService = inject(PostService);
 
+    // posts is the object that is displayed in the template
     posts: WritableSignal<Post[]> = signal([]);
 
     postToEdit: WritableSignal<Post | null> = signal(null);
+    refresh: WritableSignal<number> = signal(0);
 
     constructor() {
-        this.postService.list().subscribe((posts) => {
-            this.posts.set(posts);
+        // Single location to wire all inputs to the posts
+        effect(() => {
+            this.refresh(); // Effect input to trigger
+            this.postService.list().subscribe((posts) => {
+                this.posts.set(posts);
+            });
         });
     }
 
@@ -32,6 +38,9 @@ export class PostsComponent {
     }
 
     update(post: Post) {
-        this.postService.update(post).subscribe();
+        this.postService.update(post).subscribe(() => {
+            this.refresh.update(count => ++count); // Send event to trigger refresh
+            this.postToEdit.set(null); // Closes dialog
+        });
     }
 }
